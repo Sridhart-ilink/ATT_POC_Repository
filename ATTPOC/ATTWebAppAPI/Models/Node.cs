428a691281362596fff731395706b7dcfab69f25
Sridhar Thirumal <sridhart@ilink-systems.com>
2017-05-31 16:02:00 +0530

    Added Node class and publish properties

---
+++ ATTPOC/ATTWebAppAPI/Models/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ATTWebAppAPI.Models
+{
+    public class Node
+    {
+        public int NodeId { get; set; }
+        public int SarfId { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public string AtollSiteName { get; set; }
+        public string iPlanJobNumber { get; set; }
+        public string PaceNumber { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
