6450447e92445a74a1cee00a5f216f9daf941442
sheikanas <sheikanasa@ilink-systems.com>
2017-06-02 20:29:34 +0530

    commiting web and global files to avoid conflict

--- ATTPOC/ATTPOC/Scripts/globals.js
+++ ATTPOC/ATTPOC/Scripts/globals.js
@@ -3,6 +3,7 @@ var events = [];
 //url path declarations
 var appUrl = "http://localhost:54987/";
 var camundaBaseApiUrl = "http://localhost:55537/api/";
+//var camundaBaseApiUrl = "http://attpoc-websvr.cloudapp.net/ATTWebAppAPI/api/";
 /*
 var appUrl = "http://attpoc-websvr.cloudapp.net/ATTWorkflowPOC";
 var camundaBaseApiUrl = "http://attpoc-websvr.cloudapp.net/ATTWebAppAPI/api/";
